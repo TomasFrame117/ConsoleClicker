1696edfa70851b9691311e6a568a27cf43d20efa
tomas <tomas@DESKTOP-MV6VH7K>
2022-05-20 15:45:27 +0200

    Add project files.

---
+++ ConsoleClicker/Clicker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace ConsoleClicker
+{
+    class Clicker
+    {
+        //Properties
+        public int Count { get; set; }
+        public readonly ConsoleKey _key;
+        //Constructer
+        public Clicker(ConsoleKey key)
+        {
+            _key = key;
+            Count = 0;
+        }
+        //Metheds
+        public void Click(ConsoleKey key)
+        {
+            if(key == _key)
+            {
+                Count++;
+            }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Press {_key} Poengsum: {Count}");
+        }
+    }
+}
---
+++ ConsoleClicker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleClicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Clicker(ConsoleKey.Q);
+            Console.WriteLine("Press Q to play");
+            game.Show();
+            while (true)
+            {
+                var input = Console.ReadKey();
+                game.Click(input.Key);
+                Console.Clear();
+                game.Show();
+            }
+        }
+    }
+}
